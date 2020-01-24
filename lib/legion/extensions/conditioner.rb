b0033e2bc85d1133fb3828cefbe93154e6b7e64a
Esity <matthewdiverson@gmail.com>
2020-01-24 17:02:00 -0600

    removing idea folder

---
+++ lib/legion/extensions/conditioner.rb
@@ -0,0 +1,10 @@
+require "legion/extensions/conditioner/version"
+
+module Legion
+  module Extensions
+    module Conditioner
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/legion/extensions/conditioner/version.rb
@@ -0,0 +1,7 @@
+module Legion
+  module Extensions
+    module Conditioner
+      VERSION = "0.1.0"
+    end
+  end
+end
