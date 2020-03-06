8792c1fbb792d4ed801b9f6c5c758aee936e818c
miverso2 <matt.iverson@optum.com>
2020-03-05 22:30:21 -0600

    push

--- lib/legion/extensions/conditioner/transport.rb
+++ lib/legion/extensions/conditioner/transport.rb
@@ -2,14 +2,12 @@ require 'legion/extensions/transport'
 
 module Legion::Extensions::Conditioner
   module Transport
-    module AutoBuild
-      extend Legion::Extensions::Transport
-      def self.additional_e_to_q
-        [
+    extend Legion::Extensions::Transport
+    def self.additional_e_to_q
+      [
           { from: 'task', to: 'conditioner', routing_key: 'task.subtask' },
           { from: 'task', to: 'conditioner', routing_key: 'task.subtask.conditioner' }
-        ]
-      end
+      ]
     end
   end
 end
